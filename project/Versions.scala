f46cf43b74ae28dc5224323bb4b701d78f0d7414
Johannes Coetzee <jcoetzee@qwiet.ai>
2026-02-20 16:38:29 +0100

    Bump lombok (#5835)
    Co-authored-by: Johannes Coetzee <johannes.coetzee@harness.io>

--- project/Versions.scala
+++ project/Versions.scala
@@ -23,7 +23,7 @@ object Versions {
   val jlhttp                 = "3.1"
   val jRuby                  = "9.4.9.0"
   val json4s                 = "4.0.7"
-  val lombok                 = "1.18.32"
+  val lombok                 = "1.18.42"
   val mavenArcheologist      = "0.0.10"
   val phpParser              = "4.15.10"
   val pPrint                 = "0.8.1"
